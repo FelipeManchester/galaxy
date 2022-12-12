e8482a00ecf1d155060afdd893db2018badb0543
Felipe Manchester <f.p2809@gmail.com>
2022-12-12 19:29:03 -0300

    feat: hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,78 +5,93 @@ import { useAuthentication } from '../hooks/useAuthentication';
 import { useAuthValue } from '../contexts/AuthContext';
 
 import styles from './Navbar.module.css';
+import { useState } from 'react';
+
+import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 
 const Navbar = () => {
   const { user } = useAuthValue();
   const { logout } = useAuthentication();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
     <nav className={styles.navbar}>
       <NavLink to="/" className={styles.brand}>
         gala<span>x</span>y
       </NavLink>
-      <ul className={styles.links_list}>
-        <li>
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? styles.active : '')}
-          >
-            Home
-          </NavLink>
-        </li>
-        {!user && (
-          <>
-            <li>
-              <NavLink
-                to="/login"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
-                Entrar
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/register"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
-                Cadastrar
-              </NavLink>
-            </li>
-          </>
-        )}
-        {user && (
-          <>
-            <li>
-              <NavLink
-                to="/posts/create"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
-                Novo post
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/dashboard"
-                className={({ isActive }) => (isActive ? styles.active : '')}
-              >
-                Perfil
-              </NavLink>
-            </li>
-          </>
-        )}
-        <li>
-          <NavLink
-            to="/about"
-            className={({ isActive }) => (isActive ? styles.active : '')}
-          >
-            Sobre
-          </NavLink>
-        </li>
-        {user && (
+
+      <button onClick={toggleMenu} className={styles.hamburger}>
+        {menuOpen ? <CloseRoundedIcon /> : <MenuRoundedIcon />}
+      </button>
+      {menuOpen && (
+        <ul className={styles.links_list}>
           <li>
-            <button onClick={logout}>Sair</button>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Home
+            </NavLink>
           </li>
-        )}
-      </ul>
+          {!user && (
+            <>
+              <li>
+                <NavLink
+                  to="/login"
+                  className={({ isActive }) => (isActive ? styles.active : '')}
+                >
+                  Entrar
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/register"
+                  className={({ isActive }) => (isActive ? styles.active : '')}
+                >
+                  Cadastrar
+                </NavLink>
+              </li>
+            </>
+          )}
+          {user && (
+            <>
+              <li>
+                <NavLink
+                  to="/posts/create"
+                  className={({ isActive }) => (isActive ? styles.active : '')}
+                >
+                  Novo post
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/dashboard"
+                  className={({ isActive }) => (isActive ? styles.active : '')}
+                >
+                  Perfil
+                </NavLink>
+              </li>
+            </>
+          )}
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? styles.active : '')}
+            >
+              Sobre
+            </NavLink>
+          </li>
+          {user && (
+            <li>
+              <button onClick={logout}>Sair</button>
+            </li>
+          )}
+        </ul>
+      )}
     </nav>
   );
 };
